15c4150129dd924b960ed2539e1050bb865ec54e
ELHEND <alhnd683@gmail.com>
2023-04-23 04:22:59 -0700

    xxx

--- get_size.c
+++ get_size.c
@@ -3,21 +3,22 @@
  * get_size - Calculates the size to cast the argument
  *
  * @format: Formatted string in which to print the arguments
- * @m: List of arguments to be printed.
+ * @i: List of arguments to be printed.
  * Return: Precision.
  */
-int get_size(const char *format, int *m)
+int get_size(const char *format, int *i)
 {
-int curr_m = *m + 1;
 int size = 0;
-if (format[curr_m] == 'l')
+if (format[*i + 1] == 'l')
+{
 size = S_LONG;
-else if (format[curr_m] == 'h')
+(*i)++;
+
+}
+else if (format[*i + 1] == 'h')
+{
 size = S_SHORT;
-if (size == 0)
-*i = curr_m - 1;
-else
-*i = curr_m;
+(*i)++;
+}
 return (size);
 }
-
